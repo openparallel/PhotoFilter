64cf253470fa515dfc6d3a7f80e677dd0978aba3
BeauJoh <beau@inbeta.org>
2012-05-17 19:35:08 +1000

    still pretty initial

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,3 @@
 APP_BUILD_SCRIPT := $(call my-dir)/Android.mk
-APP_PROJECT_PATH := $(call my-dir)/../tests/VideoEmulation
+APP_PROJECT_PATH := $(call my-dir)/project
 APP_MODULES      := cxcore cv cvaux cvml cvhighgui opencv
